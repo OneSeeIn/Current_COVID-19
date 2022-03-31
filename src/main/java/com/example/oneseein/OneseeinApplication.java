af33dde1ff48a996bfc58aebbfd6a8086af078a8
devkwonms <alstj1379@nate.com>
2022-03-31 15:37:01 +0900

    COVID 첫 구축

---
+++ src/main/java/com/example/oneseein/OneseeinApplication.java
@@ -0,0 +1,17 @@
+package com.example.oneseein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneseeinApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, Oneseein !!");
+
+		SpringApplication.run(OneseeinApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/oneseein/OneseeinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oneseein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneseeinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
