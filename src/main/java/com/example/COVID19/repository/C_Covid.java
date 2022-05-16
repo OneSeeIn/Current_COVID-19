2dc711910d316aa61be9cce756955801be30a930
꼬낄라 <77871140+Braveleftji@users.noreply.github.com>
2022-05-16 17:09:53 +0900

    Update C_Covid.java

--- src/main/java/com/example/COVID19/repository/C_Covid.java
+++ src/main/java/com/example/COVID19/repository/C_Covid.java
@@ -38,8 +38,8 @@ public class C_Covid {
 
 
     public void setData(String name, String value) {
-            if(value == null) return;
-     	    switch (name){
+	    if(value.equals("null")) return;
+        switch (name){
             case ("accDefRate"): {this.accDefRate = value; break;}
             case ("accExamCnt"): {this.accExamCnt = Integer.parseInt(value); break;}
             case ("createDt"): {this.createDt = Timestamp.valueOf(value); break;}
@@ -66,3 +66,4 @@ public class C_Covid {
     }
 
 }
+
