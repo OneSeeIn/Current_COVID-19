78039a69944f650028df2d3a29e9e07bd7f8dd4d
송지훈 <jhsong@idis.co.kr>
2023-01-10 10:08:00 +0900

    NotFound

--- Current_COVID/frontend/src/App.js
+++ Current_COVID/frontend/src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import { useRecoilValue } from "recoil";
 import { loginState } from "./store/atom";
 import Main from "./components/Main";
+import NotFound from "./components/Login copy";
 
 function App() {
   const isLoggedIn = useRecoilValue(loginState);
-  console.log(isLoggedIn);
-  return <div className="App">{isLoggedIn ? <Main /> : <Login />}</div>;
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/" element={isLoggedIn ? <Main /> : <Login />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+          {/* <Route path="/join" element={<Join />}></Route> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ Current_COVID/frontend/src/components/NotFound.js
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const NotFound = () => {
+  return <Box>404 Error</Box>;
+};
+
+export default NotFound;
